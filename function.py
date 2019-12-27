ab430942a90869d0d4450311c528e2a86a308324
Akkara Taneerat <51163217+AkkaraTaneerat@users.noreply.github.com>
2019-12-27 15:10:36 +0700

    clearn library face landmark  27/12/19

--- function.py
+++ function.py
@@ -1,373 +1,190 @@
-from imutils import face_utils
-import dlib
 import cv2
 import imutils
-import os
+import dlib
 import re
-import sys
-import numpy as np
-from matplotlib import pyplot as plt
-from PIL import Image, ImageFilter
-from keras.preprocessing.image import ImageDataGenerator
-from keras.preprocessing.image import img_to_array
-from keras.preprocessing.image import load_img
-import argparse
+from imutils import face_utils
 
 
-def readimage(p,detector,predictor,width,color,msshow,
-              face,mschoose,ir,keyd,keyl):
+class myfunction:
 
-    image = cv2.imread(ir)
-    image = imutils.resize(image, width)
+    def __init__(
 
-    r = input(mschoose)
-
-    gray = cv2.cvtColor(image, color)
-    rects = detector(gray, 1)
-
-    for (i, rect) in enumerate(rects):
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
+        self,
+        detection,
+        prediction,     
+        read,
+        count,
+        keyd,
+        keyl,
+        keyq,
+        write,
+        newmodel,
+        oldmodel,
+        test,
         
-        if r == keyd:
-            cv2.imshow(msshow, image)
+        ):
 
-        (x, y, w, h) = face_utils.rect_to_bb(rect)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        self.detection = detection
+        self.prediction = prediction
+        self.read = read
+        self.count = count
+        self.keyd = keyd
+        self.keyl = keyl
+        self.keyq = keyq
+        self.write = write
+        self.newmodel = newmodel
+        self.oldmodel = oldmodel
+        self.test = test
 
-	    # show the face number
-        cv2.putText(image, face .format(i + 1), (x - 10, y - 10),
-	    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        # loop over the (x, y)-coordinates for the facial landmarks
-        # and draw them on the image
-        for (x, y) in shape:
-            cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
-            if r == keyl:
-               cv2.imshow(msshow, image)
+    def readimage(self):
 
-        # show the output image with the face detections + facial landmarks
-        
-    cv2.waitKey(0)
+        image = self.read
+        receive = input("please select d or l: ")
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        detect = self.detection(gray, 1)
 
-def showlandmark(p,detector,predictor,width,color,keyq,
-                 keyl,extension,msshow,id,ir,iwl):
-    
-    image = cv2.imread(ir)
-    image = imutils.resize(image, width)
-    gray = cv2.cvtColor(image, color)
-
-    rects = detector(gray, 1)
-    img_id = id
-
-    while True:
-
-        img_id += 1
-        pressedKey = cv2.waitKey(1) & 0xFF
-        for (i, rect) in enumerate(rects):
+        for (i, rectangle) in enumerate(detect):
 
             # determine the facial landmarks for the face region, then
             # convert the facial landmark (x, y)-coordinates to a NumPy
             # array
-            shape = predictor(gray, rect)
+            shape = self.prediction(gray, rectangle)
             shape = face_utils.shape_to_np(shape)
-    
-            (x, y, w, h) = face_utils.rect_to_bb(rect)
+
+            #show the output image with the face detections
+            if receive == self.keyd:
+                cv2.imshow("face detections", image) 
+
+            (x, y, w, h) = face_utils.rect_to_bb(rectangle)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            # show the face number
+            cv2.putText(image, "face #{}".format(i + 1), (x - 10, y - 10),
+	        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
             # loop over the (x, y)-coordinates for the facial landmarks
             # and draw them on the image
             for (x, y) in shape:
                 cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
-            # show the output image with the face detections + facial landmarks
-        if pressedKey == ord(keyl):
-            cv2.imwrite(iwl +str(img_id)+extension, image)
-        elif pressedKey == ord(keyq):
-            break
+                
+                # show the output image with the facial landmarks
+                if receive == self.keyl:
+                    cv2.imshow("face landmarks", image)
 
-        cv2.imshow(msshow, image)
+        cv2.waitKey(0)
 
-    cv2.destroyAllWindows()
-    cv2.waitKey(0)
 
-def detectface(p,detector,predictor,color,keyq,keyl,extension,
-               face,msshow,keyd,read,id,iwd,iwl):
+    def showlandmark(self):
 
-    cap = read
-    img_id = id
-    while True:
-        
-        # load the input image and convert it to grayscale
-        _, frame = cap.read()
-        gray = cv2.cvtColor(frame, color)
-        
-        # detect faces in the grayscale image
-        rects = detector(gray, 0)
-        pressedKey = cv2.waitKey(1) & 0xFF
-        # loop over the face detections
+        image = self.read
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        detect = self.detection(gray, 1)
+        img_id = self.count
 
-        for (i, rect) in enumerate(rects):
+        while True:
+
+            img_id += 1
+            pressedKey = cv2.waitKey(1) & 0xFF
+
+            for (i, rectangle) in enumerate(detect):
+                
+                # determine the facial landmarks for the face region, then
+                # convert the facial landmark (x, y)-coordinates to a NumPy
+                # array
+                shape = self.prediction(gray, rectangle)
+                shape = face_utils.shape_to_np(shape)
+
+                (x, y, w, h) = face_utils.rect_to_bb(rectangle)
+
+                # loop over the (x, y)-coordinates for the facial landmarks
+                # and draw them on the image
+                for (x, y) in shape:
+                    cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
+                 
+            if pressedKey == ord(self.keyl):
+                cv2.imwrite(self.write +str(img_id)+".jpg", image)
+            elif pressedKey == ord(self.keyq):
+                break
+
+            # show the output image with the plot face detections
+            cv2.imshow("Output", image)
+
+        cv2.destroyAllWindows()
+        cv2.waitKey(0)
+
+
+    def detectface(self):
+
+        cap = self.read
+        img_id = self.count
+
+        while True:
             
-            # determine the facial landmarks for the face region, then
-            # convert the facial landmark (x, y)-coordinates to a NumPy
-            # array
-            shape = predictor(gray, rect)
-            shape = face_utils.shape_to_np(shape)
+            # load the input image and convert it to grayscale
+            _, frame = cap.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            if pressedKey == ord(keyd):
-                cv2.imwrite(iwd +str(img_id)+extension, frame)
+            # detect faces in the grayscale image
+            detect = self.detection(gray, 0)
+            pressedKey = cv2.waitKey(1) & 0xFF
+
+            # loop over the face detections
+            for (i, rectangle) in enumerate(detect):
+                
+                # determine the facial landmarks for the face region, then
+                # convert the facial landmark (x, y)-coordinates to a NumPy
+                # array
+                shape = self.prediction(gray, rectangle)
+                shape = face_utils.shape_to_np(shape)
+
+                if pressedKey == ord(self.keyd):
+                    cv2.imwrite(self.write +str(img_id)+".jpg", frame)
                 img_id += 1
 
-            (x, y, w, h) = face_utils.rect_to_bb(rect)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                (x, y, w, h) = face_utils.rect_to_bb(rectangle)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-	        # show the face number
-            cv2.putText(frame, face.format(i + 1), (x - 10, y - 10),
-		    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            # loop over the (x, y)-coordinates for the facial landmarks
-            # and draw them on the image
-            for (x, y) in shape:
-                cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
-            # show the output image with the face detections + facial landmarks
-        if pressedKey == ord(keyl):
-            cv2.imwrite(iwl +str(img_id )+extension, frame)
-            img_id += 1
-        elif pressedKey == ord(keyq):
-            break
+                # show the face number
+                cv2.putText(frame, "face #{}".format(i + 1), (x - 10, y - 10),
+	            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        cv2.imshow(msshow, frame)
-        
-    
-    cv2.destroyAllWindows()
-    cap.release()
-    
-def checkerror(mserror,repart,renum,dir,newmodel,dlibmodel,filexml):
+                # loop over the (x, y)-coordinates for the facial landmarks
+                # and draw them on the image
+                for (x, y) in shape:
+                    cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
 
-    REG_PART = re.compile(repart)
-    REG_NUM = re.compile(renum)
+            if pressedKey == ord(self.keyl):
+                cv2.imwrite(self.write +str(img_id )+".jpg", frame)
+                img_id += 1
 
-    # dataset path
-    ibug_dir = dir
- 
-    def measure_model_error(model, xml_annotations):
-        '''requires: the model and xml path.
-        It measures the error of the model on the given
-        xml file of annotations.'''
-        error = dlib.test_shape_predictor(xml_annotations, model)
-        print(mserror.format(model, error))
+            elif pressedKey == ord(self.keyq):
+                break
 
-
-    measure_model_error(newmodel, filexml)
-    measure_model_error(dlibmodel, filexml)
-
-
-    def test(image_path, model_path):
-        '''Test the given model by showing the detected landmarks.
-            - image_path: the path of an image. Should contain a face.
-            - model_path: the path of a shape predictor model.
-        '''
-        image = cv2.imread(image_path)
-        face_detector = dlib.get_frontal_face_detector()
-        dets = face_detector(image, 1)
-        predictor = dlib.shape_predictor(model_path)
-
-        for d in dets:
-            cv2.rectangle(image, (d.left(), d.top()), (d.right(), d.bottom()), 255, 1)
-            shape = predictor(image, d)
-            for i in range(shape.num_parts):
-                p = shape.part(i)
-                cv2.circle(image, (p.x, p.y), 2, 255, 1)
-                cv2.putText(image, str(i), (p.x + 4, p.y), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255, 255, 255))
-
-        
-        cv2.waitKey(0)
+            # show the output image with the face detections
+            cv2.imshow("Output", frame)
+         
         cv2.destroyAllWindows()
+        cap.release()
+        
 
-def replacevalue(dir,rpwhat,rpwith,readpath):
+    def measure_model_error(self):
 
-    my_dir = dir
-    replace_what = rpwhat
-    replace_with = rpwith
+        # requires: the model and xml path.
+        # It measures the error of the model on the given
+        # xml file of annotations.
+        error1 = dlib.test_shape_predictor(self.test, self.newmodel)
+        error2 = dlib.test_shape_predictor(self.test, self.oldmodel)
+        print("error of the model: {} is {}".format(self.newmodel, error1))
+        print("error of the model: {} is {}".format(self.oldmodel, error2))
 
-    # loop through all files in directory
-    for fn in os.listdir(my_dir):
-    #print(fn)
-        pathfn = os.path.join(my_dir,fn)
-        if os.path.isfile(pathfn):
-            file = open(pathfn, readpath)
-            new_file_content=''
-        for line in file:
-            p = re.compile(replace_what)
-            new_file_content += p.sub(replace_with, line)
-        file.seek(0)
-        file.truncate()
-        file.write(new_file_content)
-        file.close()
-
-def trainmodel(mserror,repart,renum,eye,nose,face,landmark,train,test,opt,tree,nu,
-               cascade,feature,ntest,osamount,ostranslation,be,nthreads,namemodel):
-
-    # regex:
-    REG_PART = re.compile(repart)
-    REG_NUM = re.compile(renum)
-
-    # landmarks subsets (relative to 68-landmarks):
-    EYE_EYEBROWS = eye
-    NOSE_MOUTH = nose
-    FACE_CONTOUR = face
-    ALL_LANDMARKS = landmark
+myfunction.close()
 
 
-    # annotations
-    train_labels = train
-    test_labels = test
+          
 
-    def slice_xml(in_path, out_path, parts):
-        '''creates a new xml file stored at [out_path] with the desired landmark-points.
-        The input xml [in_path] must be structured like the ibug annotation xml.'''
-        file = open(in_path, "r")
-        out = open(out_path, "w")
-        pointSet = set(parts)
 
-        for line in file.readlines():
-            finds = re.findall(REG_PART, line)
 
-            # find the part section
-            if len(finds) <= 0:
-                out.write(line)
-            else:
-                # we are inside the part section 
-                # so we can find the part name and the landmark x, y coordinates
-                name, x, y = re.findall(REG_NUM, line)
-
-                # if is one of the point i'm looking for, write in the output file
-                if int(name) in pointSet:
-                    out.write(f"      <part name='{name}' x='{x}' y='{y}'/>\n")
-
-        out.close()
-
-    def train_model(name, xml):
-        '''requires: the model name, and the path to the xml annotations.
-        It trains and saves a new model according to the specified 
-        training options and given annotations'''
-        # get the training options
-        options = opt
-        options.tree_depth = tree
-        options.nu = nu
-        options.cascade_depth = cascade
-        options.feature_pool_size = feature
-        options.num_test_splits = ntest
-        options.oversampling_amount = osamount
-        options.oversampling_translation_jitter = ostranslation
-
-        options.be_verbose = be  # tells what is happening during the training
-        options.num_threads = nthreads   # number of the threads used to train the model
-  
-        # finally, train the model
-        dlib.train_shape_predictor(xml, name, options)
-
-    def measure_model_error(model, xml_annotations):
-        '''requires: the model and xml path.
-        It measures the error of the model on the given
-        xml file of annotations.'''
-        error = dlib.test_shape_predictor(xml_annotations, model)
-        print(mserror.format(model, error))
-
-    # -----------------------------------------------------------------------------
-    # -- Model Generation
-    # -----------------------------------------------------------------------------
-
-    # add or remove models here.
-    models = [
-     # pair: model name, parts
-    (namemodel, ALL_LANDMARKS)
-    ]
-
-    for model_name, parts in models:
-        print(f"processing model: {model_name}")
-  
-        train_xml = f"{model_name}_train.xml"
-        test_xml = f"{model_name}_test.xml"
-        dat = f"{model_name}.dat"
-        slice_xml(train_labels, train_xml, parts)
-        slice_xml(test_labels, test_xml, parts)
-  
-        print("train")
-        # training
-        train_model(dat, train_xml)
  
-        # compute traning and test error
-        measure_model_error(dat, train_xml)
-        measure_model_error(dat, test_xml)
+    
 
-        # -----------------------------------------------------------------------------
-
-    def test(image_path, model_path):
-        '''Test the given model by showing the detected landmarks.
-            - image_path: the path of an image. Should contain a face.
-            - model_path: the path of a shape predictor model.
-        '''
-        image = cv2.imread(image_path)
-        face_detector = dlib.get_frontal_face_detector()
-        dets = face_detector(image, 1)
-        predictor = dlib.shape_predictor(model_path)
-
-        for d in dets:
-            cv2.rectangle(image, (d.left(), d.top()), (d.right(), d.bottom()), 255, 1)
-            shape = predictor(image, d)
-            for i in range(shape.num_parts):
-                p = shape.part(i)
-                cv2.circle(image, (p.x, p.y), 2, 255, 1)
-                cv2.putText(image, str(i), (p.x + 4, p.y), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255, 255, 255))
-
-        cv2.imshow("window", image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-def imagejitter(default,msload,rotation,zoom,width,height,shear,status,mode,
-                total,msgenerat,file):
-
-    # construct the argument parser and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", required=True,
-	    help="path to the input image")
-    ap.add_argument("-o", "--output", required=True,
-	    help="path to output directory to store augmentation examples")
-    ap.add_argument("-t", "--total", type=int, default=default,
-	    help="# of training samples to generate")
-    args = vars(ap.parse_args())
-
-    # load the input image, convert it to a NumPy array, and then
-    # reshape it to have an extra dimension
-    print(msload)
-    image = load_img(args["image"])
-    image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-
-    # construct the image generator for data augmentation then
-    # initialize the total number of images generated thus far
-    aug = ImageDataGenerator(
-	    rotation_range = rotation,
-	    zoom_range = zoom,
-	    width_shift_range = width,
-	    height_shift_range = height,
-	    shear_range = shear,
-	    horizontal_flip = status,
-	    fill_mode = mode)
-    total = total
-
-    # construct the actual Python generator
-    print(msgenerat)
-    imageGen = aug.flow(image, batch_size=1, save_to_dir=args["output"],
-	    save_prefix="image", save_format=file)
-
-    # loop over examples from our image data augmentation generator
-    for image in imageGen:
-	    # increment our counter
-	    total += 1
-
-	    # if we have reached the specified number of examples, break
-	    # from the loop
-	    if total == args["total"]:
-		    break
 
--- home.py
+++ home.py
@@ -1,145 +1,21 @@
-import function
-import cv2
+from cleanfunction import myfunction
 import dlib
+import cv2
+import numpy as np
 
-#detection
-p = "face_landmarks_18-11-62.dat";
-detector = dlib.get_frontal_face_detector();
-predictor = dlib.shape_predictor(p);
-#resize
-width = 500;
-#color
-color = cv2.COLOR_BGR2GRAY;
-#key exit
-keyq = ('q');
-#key save image landmark
-keyl = ('l');
-#print error model
-mserror = "error of the model: {} is {}";
-#message imshow
-msshow = "output";
-#message count face
-face = "face #{}";
-#chang File extension
-extension = ".jpg";
+p = myfunction(
 
+    detection = dlib.get_frontal_face_detector(),
+    prediction = dlib.shape_predictor("file/face_landmarks_25-12-62.dat"),
+    read = cv2.VideoCapture("video/1.avi"),     #cv2.VideoCapture("video/1.avi") && cv2.imread("pic/1.jpg")
+    count = 1,                                  #showlandmark count = 0 && detectface count = 1
+    keyd = ('d'),
+    keyl = ('l'),
+    keyq = ('q'),
+    write = "video/saveimage/ve",                 #picture "pic/saveimage/img" && "video/saveimage/ve"
+    newmodel = "face_landmarks_25-12-62.dat",
+    oldmodel = "face_landmarks_19-12-62.dat",
+    test = f"face_landmarks_test.xml",
 
-#functuion readimage
-"""function.readimage (
-    p,
-    detector,
-    predictor,
-    width,
-    color,
-    msshow,
-    face,
-    mschoose = "please choose d|l: ",   #print choose
-    ir = "test/te1.jpg",                #imread image
-    keyd = ('d'),                       #imshow image detection
-    keyl = ('l')                        #imshow image landmark
-    
-    );"""
+    )
 
-#function showlandmark
-"""function.showlandmark (
-    p,
-    detector,
-    predictor,
-    width, 
-    color,
-    keyq,
-    keyl,
-    extension,
-    msshow,
-    id = 0,                             #img_id
-    ir = "picture/train/t5.jpg",        #imread image 
-    iwl = "picture/savelandmark/la."    #imwrite image landmark
-
-    );"""
-
-#function detectface
-"""function.detectface (
-    p,
-    detector,
-    predictor,
-    color,
-    keyq,
-    keyl,
-    extension,
-    face,
-    msshow,
-    keyd = ('d'),                    #key save image detection
-    read = cv2.VideoCapture(0),
-    id = 1,                          #img_id
-    iwd = "picture/detection/de.",   #imwrite image detection
-    iwl = "picture/landmark/la."     #imwrite image landmark
-
-    );"""
-
-#function checkerror
-"""function.checkerror(
-    mserror,                                          #print error model
-    repart = "part name='[0-9]+'",                    #REG_PART
-    renum = "[0-9]+",                                 #REG_NUM
-    dir = "face_landmark",                            #ibug_dir
-    newmodel = "face_landmarks_18-11-62.dat",         #file .dat new model
-    dlibmodel = "face_landmarks_68_30.dat",           #file .dat dlib model
-    filexml = f"face_landmarks_18-11-62_test.xml"     #file .xml test&train model
-
-    );"""
-
-#function replacevalue
-"""function.replacevalue(
-    dir = 'C://Users//FRONTIS//OneDrive - Frontis//face_landmark//filereplace',   #my_dir
-    rpwhat = "name='0'",     #replacewhat
-    rpwith = "name='00'",    #replacewith
-    readpath = 'r+'          #file open
-
-    );"""
-
-#function trainmodel
-"""function.trainmodel(
-    mserror,                                                #print error model
-    repart = "part name='[0-9]+'",                          #REG_PART
-    renum = "[0-9]+",                                       #REG_NUM
-    eye = [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 36,      #EYE_EYEBROWS
-           37, 38, 39,40, 41, 42, 43, 44, 45, 46, 47],
-    nose = [27, 28, 29, 30, 31, 32, 33, 34, 35, 48, 49,     #NOSE_MOUTH
-            50, 51, 52,53, 54, 55, 56, 57, 58, 59, 60, 
-            61, 62, 63, 64, 65, 66, 67],
-    face = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,   #FACE_CONTOUR
-            14, 15, 16],  
-    landmark = range(0, 68),                                #ALL_LANDMARKS           
-    train = "labels_ibug_300W_train.xml",                   #file .xml train model
-    test = "face_landmarks_test.xml",                       #file .xml test model
-    opt = dlib.shape_predictor_training_options(),          #option
-    tree = 3,                                               #options.tree_depth
-    nu = 0.1,                                               #options.nu
-    cascade = 10,                                           #options.cascade_depth
-    feature = 150,                                          #options.feature_pool_size
-    ntest = 350,                                            #options.num_test_splits
-    osamount = 5,                                           #options.oversampling_amount
-    ostranslation = 0,                                      #options.oversampling_translation_jitter
-    be = True,                                              #options.be_verbose
-    nthreads = 1,                                           #options.num_threads
-    namemodel = "face_landmarks_68"                         #model name
-    
-    );"""
-
-#function imagejitter
- # python home.py --image t1.jpg --output generated_dataset/data
-"""function.imagejitter(
-    default = 100,                                     #default
-    msload = "[INFO] loading example image...",        #message load the input image
-    rotation = 0.2,                                    #rotation_range      
-    zoom = 0.10,                                       #zoom_range
-    width = 0.1,                                       #width_shift_range
-    height = 0.1,                                      #height_shift_range
-    shear = 0.10,                                      #shear_range
-    status = True,                                     #horizontal_flip
-    mode = "nearest",                                  #fill_mode 
-    total = 0,                                         #total
-    msgenerat = "[INFO] generating images...",         #message generator
-    file = "jpg"                                       #save_format 
-
-);"""
